3ec5caee224d5ff25d2a9b413b5a2d2ddd9be43d
Leeya Davis <leeya463@gmail.com>
2019-03-03 19:07:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,11 @@
 def my_select(collection) #{ |element| element.even?}
  if block_given?
    b = 0
-   new_collection = []#No empty array to shovel transformed data into as #select returns new data-visibility
+   #No empty array to shovel transformed data into as #select returns new data-visibility
    while b < collection.length
-     new_collection << yield(collection[b])
+     yield(collection[b])
      b += 1
    end
-   new_collection
  else
    puts "This block should not run!"
  end
