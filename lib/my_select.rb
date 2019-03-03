aa9d2c2a96b211d3edd5df6652281627bb1e20f6
Leeya Davis <leeya463@gmail.com>
2019-03-03 18:25:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(collection)
  if block_given?
    b = 0
-   collect = [] #No empty array to shovel transformed data into as #select returns new data-visibility
+    #No empty array to shovel transformed data into as #select returns new data-visibility
    while b < collection.length
-     collect << yield(collection[b]) #yield(argument isolating array element).find_all
+      yield(collection[b]) #yield(argument isolating array element).find_all
      b += 1
    end
-   collect
+
  else
    puts "This block should not run!"
  end
