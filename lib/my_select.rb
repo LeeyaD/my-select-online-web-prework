0706fbcff50fd14b32fe9012b0f75bd70d9951df
Leeya Davis <leeya463@gmail.com>
2019-03-03 18:42:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection) #{ |element| element.even?}
    b = 0
    new_collection = []#No empty array to shovel transformed data into as #select returns new data-visibility
    while b < collection.length
-     new_collection << yield(collection[b]) == true
+     new_collection << yield(collection[b])
      b += 1
    end
    new_collection
