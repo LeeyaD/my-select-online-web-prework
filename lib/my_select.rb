18ef7adfc3f568f8a646bef93a0d5d5aece8a292
Leeya Davis <leeya463@gmail.com>
2019-03-03 18:22:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  b = 0
  collect = [] #No empty array to shovel transformed data into as #select returns new data-visibility
  while b < collection.length
-   collect << yield(collection[b].true?) #yield(argument isolating array element).find_all
+   collect << yield(collection[b]) #yield(argument isolating array element).find_all
    b += 1
  end
  collect
