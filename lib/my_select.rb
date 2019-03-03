a954100e412fa523bde00dabe23a0168c7617db8
Leeya Davis <leeya463@gmail.com>
2019-03-03 19:14:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
-require 'pry'
+
 def my_select(collection) #{ |element| element.even?}
  if block_given?
    b = 0
    new_collection = []#No empty array to shovel transformed data into as #select returns new data-visibility
    while b < collection.length
      new_collection << yield(collection[b])
-     binding.pry
      b += 1
    end
    new_collection
