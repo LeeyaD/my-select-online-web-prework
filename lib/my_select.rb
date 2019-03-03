a491d10cb1a758bccd04270b57b495d5813bd712
Leeya Davis <leeya463@gmail.com>
2019-03-03 18:19:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(collection)
- b = 0 #initializing code for while loop
- #No empty array to shovel transformed data into as #select returns new data-visibility
- while b < collection.length #while loop + condition
-   yield(collection[b])#yield(argument isolating array element).find_all
+ b = 0  #initializing code for while loop
+ collect = [] #No empty array to shovel transformed data into as #select returns new data-visibility
+ while b < collection.length  #while loop + condition
+   yield(collection[b]) #yield(argument isolating array element).find_all
    b += 1#change code
  end#end of loop
-
+ collect
 end
